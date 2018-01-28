965895073429732da5bbb7f4ed775077384c82c4
DominicHolmes <dominic.f.holmes@gmail.com>
2018-01-27 19:50:05 -0500

    notification tab, login screen UI stuff

---
+++ Pear/NotificationsViewController.swift
@@ -0,0 +1,55 @@
+//
+//  NotificationsViewController.swift
+//  Pear
+//
+//  Created by Dominic Holmes on 1/27/18.
+//  Copyright © 2018 Dominic Holmes. All rights reserved.
+//
+
+import UIKit
+
+class NotificationsViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+// MARK: - TableView Data Source
+extension NotificationsViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 15
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "NotificationCell")
+        
+        let notificationDescriptionLabel = cell?.viewWithTag(100) as? UILabel
+        notificationDescriptionLabel?.text = "New app feature from your friends at Pear!"
+        
+        return cell!
+    }
+}
+
+// MARK: - TableView Delegate
+extension NotificationsViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        // segue to correct profile
+    }
+}
+
+// MARK: - Segue Control
+extension NotificationsViewController {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+}
