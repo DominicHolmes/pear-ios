1815828bb5eee3122a09c07197c1eb66c1fbc223
dom <hello@dom.voyage>
2018-05-01 16:27:46 -0400

    created custom TabBarController to pass active user data more effectively

---
+++ Pear/General/PearTabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  PearTabBarController.swift
+//  Pear
+//
+//  Created by dominic on 5/1/18.
+//  Copyright © 2018 Dominic Holmes. All rights reserved.
+//
+
+import UIKit
+
+class PearTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.selectedIndex = 2
+    }
+}
