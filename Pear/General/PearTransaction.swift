e41272b495e90a360ae3372317db3a2e57bf2520
dom <hello@dom.voyage>
2018-05-17 17:17:22 -0500

    work on transaction object

--- Pear/General/PearTransaction.swift
+++ Pear/General/PearTransaction.swift
@@ -7,3 +7,48 @@
 //
 
 import Foundation
+
+// Primary user is the whoever initiates the Pear (through scanning, username, etc.)
+
+enum PearTransactionPerspective {
+    case primary
+    case secondary
+}
+
+enum PearTransactionState {
+    case waiting
+    case approved
+}
+
+class PearTransaction {
+    
+    private var transactionState: PearTransactionState = .waiting
+    
+    private var primaryApproval: Bool = false
+    private var secondaryApproval: Bool = false
+    private var overallApproval: Bool = false
+    
+    private var primaryProfileID: Int?
+    private var secondaryProfileID: Int?
+    private var dateOfCompletion: Date?
+    
+    init(primary primaryID: Int!, secondary secondaryID: Int!, _ perspective: PearTransactionPerspective) {
+        self.primaryProfileID = primaryID
+        self.secondaryProfileID = secondaryID
+        switch perspective {
+        case .primary: primaryApproval = true
+        case .secondary: secondaryApproval = true
+        }
+        updateTransactionState()
+    }
+    
+    private func updateTransactionState() {
+        overallApproval = primaryApproval && secondaryApproval
+        switch overallApproval {
+        case true: transactionState = .approved
+        case false: transactionState = .waiting
+        }
+    }
+    
+    
+}
