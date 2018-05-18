aded79dc5714fc258432547494d74dee8b9619b4
dom <hello@dom.voyage>
2018-05-18 18:20:17 -0500

    refactoring

--- Pear/General/SocialProfile.swift
+++ Pear/General/SocialProfile.swift
@@ -35,29 +35,6 @@ class SocialProfile: NSObject {
         }
         self.socialServices = loadedServices
     }
-    /*
-    if let profilesDict = snapshot.value as? [String: Any] {
-        for eachProfileId in profilesDict.keys {
-            if let eachProfile = profilesDict[eachProfileId] as? [String : String] {
-                var loadedServices = [SocialService]()
-                var profileName = "Profile"
-                let profileId = eachProfileId
-                for service in eachProfile {
-                    if service.key == "!ProfileName" {
-                        profileName = service.value
-                    } else if service.key != "!ProfileId" {
-                        let newService = SocialService(socialService: SocialServiceType(rawValue: service.key),
-                                                       handle: SocialProfile.parseHandle(service.value),
-                                                       ranking: SocialProfile.parseRanking(service.value))
-                        loadedServices.append(newService)
-                    }
-                }
-                let newProfile = SocialProfile(name: profileName, services: loadedServices)
-                newProfile.setProfileID(id: profileId)
-                loadedSocialProfiles.append(newProfile)
-            }
-        }
-    }*/
     
     func getName() -> String! {
         return profileName
