4be4b010951c718e24e1853de0d55bfb88f78f92
dominic <hello@dom.voyage>
2018-08-13 22:44:15 -0500

    partial implementation for EditProfileViewController

--- Pear/Profiles/EditProfileViewController.swift
+++ Pear/Profiles/EditProfileViewController.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2018 Dominic Holmes. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 class EditProfileViewController: PryntViewController {
     
@@ -14,5 +14,71 @@ class EditProfileViewController: PryntViewController {
         dismiss(animated: true, completion: nil)
     }
     
-    var profileToEdit: PryntProfile!
+    var profileToEdit: PryntProfile?
+    
+    
+    
+    
+}
+
+// MARK: - CollectionView Data Source
+extension EditProfileViewController: UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return (profileToEdit?.accounts?.count ?? 0) + 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "NetworkCell", for: indexPath) as! NetworkCollectionCell
+        let cellImageView = cell.viewWithTag(100) as? UIImageView
+        
+        guard let accounts = profileToEdit?.accounts, accounts.count != indexPath.row else {
+            cellImageView?.image = UIImage(named: "pear-logo")
+            cell.socialServiceType = nil
+            cell.accountToDisplay = nil
+            cell.accountToEdit = nil
+            return cell
+        }
+        
+        let account = accounts[indexPath.row]
+        cell.socialServiceType = account.service
+        cell.accountToDisplay = nil
+        cell.accountToEdit = account
+        cellImageView?.image = UIImage(named: account.service.photoName)
+        
+        return cell
+    }
+}
+
+// MARK: - CollectionView Delegate
+extension EditProfileViewController: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        guard let accounts = profileToEdit?.accounts, accounts.count != indexPath.row else {
+            performSegue(withIdentifier: "AddNewAccountsSegue", sender: nil)
+            return
+        }
+        
+        let account = accounts[indexPath.row]
+        let service = account.service
+        let username = account.handle
+        
+        let appURLString = service.appURL + username
+        let webURLString = service.webURL + username
+        
+        let appURL = NSURL(string: appURLString)!
+        let webURL = NSURL(string: webURLString)!
+        let application = UIApplication.shared
+        
+        if application.canOpenURL(appURL as URL) {
+            application.open(appURL as URL)
+        } else {
+            application.open(webURL as URL)
+        }
+    }
 }
